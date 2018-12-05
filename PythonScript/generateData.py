040da41297f99fba5fdb24d417e29a4c8b2bbb97
Kay-PC\Kay <kayrogg@yahoo.de>
2018-12-05 10:33:49 +0100

    PythonScript erweitert

--- PythonScript/generateData.py
+++ PythonScript/generateData.py
@@ -9,19 +9,29 @@ def generate_point(distribution, noise, cluster_number, number_of_clusters):
         point = [np.random.normal(), np.random.normal(), np.random.normal(), np.random.randint(max_value),
                  np.random.randint(4) * (max_value/4), np.random.randint(2) * max_value, 12345, cluster_number * 10]
     elif distribution == 'c':
-        point = [np.random.standard_cauchy(), np.random.normal()]
+        point = [np.random.standard_cauchy(), np.random.standard_cauchy(), np.random.standard_cauchy(),
+                 np.random.randint(max_value), np.random.randint(4) * (max_value / 4),
+                 np.random.randint(2) * max_value, 12345, cluster_number * 10]
     else:
-        point = [np.random.uniform(), np.random.normal()]
+        point = [np.random.uniform(), np.random.uniform(), np.random.uniform(), np.random.randint(max_value),
+                 np.random.randint(4) * (max_value / 4), np.random.randint(2) * max_value, 12345, cluster_number * 10]
 
     return point
 
 
 def generate_data(distribution, noise, number_of_clusters, points_per_cluster):
-    out_array = [generate_point(distribution, noise, math.floor(i/points_per_cluster), number_of_clusters) for i in range(points_per_cluster * number_of_clusters)]
+    out_array = [generate_point(distribution, noise, math.floor(i/points_per_cluster), number_of_clusters)
+                 for i in range(points_per_cluster * number_of_clusters)]
     return out_array
 
 
-data = generate_data('g', 0, 2, 2)
+data = generate_data(distribution='g', noise=0, number_of_clusters=3, points_per_cluster=2)
 print(data)
-kmeans = k_means(X=data, n_clusters=2)
-print(kmeans)
+kmeans = k_means(init='k-means++', X=data, n_clusters=3)
+# print(kmeans)
+# appends k_means cluster label to each point
+for i in range(len(data)):
+    data[i].append(kmeans[1][i])
+
+# print(data)
+
